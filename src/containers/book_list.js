baed48e36fe2d2e76280234319dcea1327c0f2a0
Li Lin <guru.lin@gmail.com>
2016-06-04 23:31:13 +0800

    book list loads.

---
+++ src/containers/book_list.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+class BookList extends React.Component {
+  renderList() {
+    return this.props.books.map(book => (
+      <li key={book.title} className="list-group-item">
+        {book.title}
+      </li>
+    ));
+  }
+
+  render() {
+    return (
+      <ul className="group-list col-sm-4">
+        {this.renderList()}
+      </ul>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    books: state.books
+  };
+}
+export default connect(mapStateToProps)(BookList);
---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,7 @@
+export default function () {
+  return [
+    { title: 'Structure and Interpretation of Computer Programs' },
+    { title: 'Javascript: The Good Parts' }, // eslint-disable-line
+    { title: 'The Wellgrounded Rubist' }
+  ];
+}
